19415d19d6ff91375567e7d0a44b633a36847d93
Andrew <115622656+pasosnikovandrej03@users.noreply.github.com>
2023-10-25 07:25:12 +0300

    Update Task_24.cpp

--- operatory if and switch/Task_24.cpp
+++ operatory if and switch/Task_24.cpp
@@ -2,10 +2,9 @@
 
 int main() {
     for (int number = 1000; number < 10000; number++) {
-        int first_two_digits = number / 100;  // Отримуємо перші дві цифри
-        int last_two_digits = number % 100;   // Отримуємо останні дві цифри
+        int first_two_digits = number / 100;  
+        int last_two_digits = number % 100;   /
 
-        // Перевіряємо, чи перші дві цифри дорівнюють двом іншим
         if (first_two_digits == 22) {
             std::cout << number << std::endl;
         }
