b27f0e451fcc1947f8689364c46a850a479213af
Andrew <115622656+pasosnikovandrej03@users.noreply.github.com>
2023-10-25 07:08:08 +0300

    Update Task_2.cpp

--- Programming cyclic algorithms/Task_2.cpp
+++ Programming cyclic algorithms/Task_2.cpp
@@ -5,11 +5,11 @@ int main() {
     double sum_of_roots = 0.0;
 
     for (int i = 1; i <= 20; i++) {
-        double root = sqrt(i);  // Обчислюємо корінь числа i
-        sum_of_roots += root;   // Додаємо корінь до загальної суми
+        double root = sqrt(i);  
+        sum_of_roots += root;   
     }
 
-    double average = sum_of_roots / 20;  // Знаходимо середнє арифметичне
+    double average = sum_of_roots / 20;  
 
     std::cout << "Середнє арифметичне коренів чисел від 1 до 20: " << average << std::endl;
 
