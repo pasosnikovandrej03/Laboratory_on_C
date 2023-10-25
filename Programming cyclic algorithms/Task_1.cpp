9077790c1e75ed9a1a375ce70d7a73ce1d9c4512
Andrew <115622656+pasosnikovandrej03@users.noreply.github.com>
2023-10-25 07:09:15 +0300

    Update Task_1.cpp

--- Programming cyclic algorithms/Task_1.cpp
+++ Programming cyclic algorithms/Task_1.cpp
@@ -2,16 +2,15 @@
 #include <cmath>
 
 int main() {
-    const int numValues = 5;  // Кількість значень x, які ми хочемо обчислити
+    const int numValues = 5;  
     double x, y;
 
-    std::cout << "Введіть п'ять різних значень x для обчислення функції:" << std::endl;
+    std::cout << "Enter five different x values to calculate the function:" << std::endl;
 
     for (int i = 1; i <= numValues; i++) {
-        std::cout << "Введіть значення x " << i << ": ";
+        std::cout << "Enter value x" << i << ": ";
         std::cin >> x;
 
-        // Обчислюємо значення функції для введеного x
         y = pow(sin(x), 5) + std::abs(5 * x - 1.5);
 
         std::cout << "Для x = " << x << ", y = " << y << std::endl;
