5179fc01596b9daa12077fc12afec1121e8e56bb
Andrew <115622656+pasosnikovandrej03@users.noreply.github.com>
2023-10-25 07:11:10 +0300

    Update Task_3.cpp

--- Programming cyclic algorithms/Task_3.cpp
+++ Programming cyclic algorithms/Task_3.cpp
@@ -5,11 +5,11 @@
 int main() {
     double a, b, h;
     
-    std::cout << "Введіть початок відрізку a: ";
+    std::cout << "Insert cob into cut a: ";
     std::cin >> a;
-    std::cout << "Введіть кінець відрізку b: ";
+    std::cout << "Enter the end of section b: ";
     std::cin >> b;
-    std::cout << "Введіть крок h: ";
+    std::cout << "Enter code h: ";
     std::cin >> h;
     
     std::cout << "-------------------\n";
@@ -19,12 +19,11 @@ int main() {
     for (double x = a; x <= b; x += h) {
         double y;
         
-        // Перевіряємо, чи x належить ОДЗ
         if (x * x <= 9.0) {
             y = std::pow(x, 5) - std::sqrt(9.0 - x * x);
             std::cout << std::setw(10) << x << std::setw(10) << y << "\n";
         } else {
-            std::cout << std::setw(10) << x << " : Значення x не належить ОДЗ\n";
+            std::cout << std::setw(10) << x << " : Significance x does not apply to ODZ\n";
         }
     }
     
